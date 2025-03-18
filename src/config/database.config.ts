74a42c9f9009bcf5c77b2691b3cde6f7ca6a4dab
sunniesfish <156326523+sunniesfish@users.noreply.github.com>
2025-03-18 19:23:45 +0900

    database.config.ts 업데이트

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,15 +2,14 @@ import { registerAs } from '@nestjs/config/dist/utils/register-as.util';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 
 export default registerAs('database', (): TypeOrmModuleOptions => {
-    return {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      synchronize: true,
-      logging: true,
-    };
-  }
+  return {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    logging: true,
+  };
 });
