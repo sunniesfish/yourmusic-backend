ccbb85d69aec6c643948770620ccf1be962fc3a0
sunniesfish <hhch5611.jh@gmail.com>
2025-03-18 14:57:11 +0900

    Refactor database configuration to handle production and development environments separately, ensuring appropriate settings for MySQL connection and logging.

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,16 +1,30 @@
-import { registerAs } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { registerAs } from '@nestjs/config/dist/utils/register-as.util';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 
-export default registerAs(
-  'database',
-  (): TypeOrmModuleOptions => ({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    synchronize: process.env.NODE_ENV !== 'production',
-    logging: process.env.NODE_ENV !== 'production',
-  }),
-);
+export default registerAs('database', (): TypeOrmModuleOptions => {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: false,
+      logging: false,
+      extra: {
+        socketPath: `/cloudsql/${process.env.DB_HOST}`,
+      },
+    };
+  } else {
+    return {
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    };
+  }
+});
