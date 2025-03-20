93c1f0f3ae2d2295eb202fa201858c51784af866
sunniesfish <hhch5611.jh@gmail.com>
2025-03-21 01:02:56 +0900

    deploying

--- src/app.module.ts
+++ src/app.module.ts
@@ -58,11 +58,4 @@ const envFilePath = '/secrets/.env';
   controllers: [],
   providers: [],
 })
-export class AppModule {
-  constructor(private configService: ConfigService) {
-    console.log('DB_HOST', this.configService.get('DB_HOST'));
-    if (!this.configService.get('DB_HOST')) {
-      throw new Error('DB_HOST is not set');
-    }
-  }
-}
+export class AppModule {}
