84e4ee31e8191dbd0fb36702625ada9983555308
sunniesfish <hhch5611.jh@gmail.com>
2025-03-30 20:27:25 +0900

    Remove YouTubeConfigService from YouTubeModule providers to simplify dependency management and streamline module configuration.

--- src/playlist/providers/youtube/youtube.module.ts
+++ src/playlist/providers/youtube/youtube.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { YouTubeService } from './youtube.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { ScraperModule } from '../scraper/scraper.module';
-import { YouTubeConfigService } from './client/youtubeConfig';
 import { YouTubeApiClient } from './client/youtube-api.client';
 
 @Module({
   imports: [AuthModule, ScraperModule],
-  providers: [YouTubeService, YouTubeConfigService, YouTubeApiClient],
+  providers: [YouTubeService, YouTubeApiClient],
   exports: [YouTubeService],
 })
 export class YouTubeModule {}
